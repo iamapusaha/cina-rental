b91ee0929924abdca34aed9531a8e3c4ea31e6ff
iamapusaha <apusaha392@gmail.com>
2025-11-20 11:25:27 +0600

    added header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,42 @@
+import sun from "../assets/icons/sun.svg";
+import logo from "../assets/logo.svg";
+import ring from "../assets/ring.svg";
+import cart from "../assets/shopping-cart.svg";
 export default function Header() {
-  return <h1>hello</h1>;
+  return (
+    <header>
+      <nav className="container mx-auto flex items-center justify-between space-x-10 py-6">
+        <a href="index.html">
+          <img src={logo} width="139" height="26" alt="logo" />
+        </a>
+
+        <ul className="flex items-center space-x-5">
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/7% rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={ring} width="24" height="24" alt="ring" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/7% rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={sun} width="24" height="24" alt="sun" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/7% rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={cart} width="24" height="24" alt="shopping cart" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
