f22a681252ba5f1f829edde83ecf127a3bdf8c8f
iamapusaha <apusaha392@gmail.com>
2025-11-22 15:39:15 +0600

    added dark & light mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,27 @@ import MovieList from "./components/cina/MovieList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
-import { MovieContext } from "./context";
+import { MovieContext, ThemeContext } from "./context";
 
 function App() {
   const [cartData, setCartData] = useState([]);
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <>
-      <MovieContext.Provider value={{ cartData, setCartData }}>
-        <Header />
-        <main>
-          <div className="container mx-auto grid lg:grid-cols-[218px_1fr] gap-14">
-            <SideBar />
+    <div className={`${darkMode ? "dark" : ""}`}>
+      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+        <MovieContext.Provider value={{ cartData, setCartData }}>
+          <Header />
+          <main>
+            <div className="container mx-auto grid lg:grid-cols-[218px_1fr] gap-14">
+              <SideBar />
 
-            <MovieList />
-          </div>
-        </main>
-        <Footer />
-      </MovieContext.Provider>
-    </>
+              <MovieList />
+            </div>
+          </main>
+          <Footer />
+        </MovieContext.Provider>
+      </ThemeContext.Provider>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 import { useContext, useState } from "react";
+import moon from "../assets/icons/moon.svg";
 import sun from "../assets/icons/sun.svg";
 import logo from "../assets/logo.svg";
 import ring from "../assets/ring.svg";
 import cart from "../assets/shopping-cart.svg";
-import { MovieContext } from "../context";
+import { MovieContext, ThemeContext } from "../context";
 import CardDetails from "./cartDetails";
 export default function Header() {
   const [showCart, setShowCart] = useState(false);
   const { cartData } = useContext(MovieContext);
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
   function handleShowCart(value) {
     setShowCart(value);
-    console.log(cartData);
   }
   return (
     <header>
@@ -31,10 +32,16 @@ export default function Header() {
           </li>
           <li>
             <a
+              onClick={() => setDarkMode((darkMode) => !darkMode)}
               className="bg-primary/20 dark:bg-primary/7% rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
             >
-              <img src={sun} width="24" height="24" alt="sun" />
+              <img
+                src={darkMode ? sun : moon}
+                width="24"
+                height="24"
+                alt="sun"
+              />
             </a>
           </li>
           <li>
